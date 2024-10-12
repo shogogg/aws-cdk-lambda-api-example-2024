c563f7f08f0cdd33187384a85b1b350b15b0d17a
shogogg <shogo@studiofly.net>
2024-10-13 08:21:11 +0900

    feat: Enable to cdk deploy

---
+++ bin/lambda-api-app.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib'
+import * as dotenv from 'dotenv'
+import { LambdaApiStack } from '../lib/lambda-api-stack'
+
+// Load environment variables from .env file
+dotenv.config()
+
+const account = process.env.APP_AWS_ACCOUNT ?? process.env.CDK_DEFAULT_ACCOUNT
+const region = process.env.APP_AWS_REGION ?? process.env.CDK_DEFAULT_REGION
+const appName = process.env.APP_NAME ?? 'ExampleApp'
+const hostedZoneName = process.env.APP_HOSTED_ZONE_NAME ?? 'example.com'
+
+const app = new cdk.App()
+
+new LambdaApiStack(app, `${appName}LambdaApiStack`, {
+  env: {
+    account,
+    region,
+  },
+  hostedZoneName,
+})
